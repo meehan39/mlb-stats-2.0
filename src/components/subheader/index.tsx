a0e9121543d8864f6d5447ab5e2ce7bb5d401eb4
Brendan Meehan <bmeehan@ebsco.com>
2024-03-21 15:22:56 -0400

    make subheader sticky

--- src/components/subheader/index.tsx
+++ src/components/subheader/index.tsx
@@ -6,13 +6,14 @@ import type Subheader from './types';
 export default function Subheader({ text }: Subheader.Props) {
     const router = useRouter();
     return (
-        <div className={`flex w-full justify-between items-center`}>
+        <div
+            className={`sticky top-0 flex w-full justify-between items-center rounded-t bg-neutral-950`}>
             <div
                 onClick={router.back}
                 className='rounded-lg border border-transparent p-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'>
                 <Image src='/back.svg' alt='' width={20} height={20} />
             </div>
-            <h2 className={`mb-3 text-xl font-semibold whitespace-nowrap px-4`}>
+            <h2 className={`text-xl font-semibold whitespace-nowrap px-4`}>
                 {text}
             </h2>
         </div>
