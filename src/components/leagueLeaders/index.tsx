5587345ed5be5509358885b33232814b9ffc069c
Brendan Meehan <bmeehan@ebsco.com>
2024-03-21 16:45:29 -0400

    display league leaders error

--- src/components/leagueLeaders/index.tsx
+++ src/components/leagueLeaders/index.tsx
@@ -15,34 +15,12 @@ import type LeagueLeaders from './types';
 import type LeagueLeadersApi from '../../app/api/leagueLeaders/types';
 
 export default async function LeagueLeaders() {
-    // const { data }: LeagueLeadersApi.Response =
-    //     await axios.get('/api/leagueLeaders');
+    try {
+        const { data }: LeagueLeadersApi.Response =
+            await axios.get('/api/leagueLeaders');
+        return <div>{JSON.stringify(data)}</div>;
+    } catch (e) {
+        return <div>{JSON.stringify(e)}</div>;
+    }
 
-    return (
-        <>
-            <Subheader text='League Leaders' />
-            <TableContainer component={Paper}>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Player</TableCell>
-                            <TableCell>Owner</TableCell>
-                            <TableCell align='right'>Home Runs</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {/* {data.map(({ playerId, fullName, owner, homeRuns }) => (
-                            <LeagueLeaderRow
-                                key={playerId}
-                                playerId={playerId}
-                                fullName={fullName}
-                                teamKey={owner}
-                                homeRuns={homeRuns}
-                            />
-                        ))} */}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </>
-    );
 }
