6e5d6bae7dd6919d71274fc7b3d5a96e9f23b60b
Brendan Meehan <bmeehan@ebsco.com>
2024-03-21 16:51:32 -0400

    re add league leaders

--- src/components/leagueLeaders/index.tsx
+++ src/components/leagueLeaders/index.tsx
@@ -15,12 +15,39 @@ import type LeagueLeaders from './types';
 import type LeagueLeadersApi from '../../app/api/leagueLeaders/types';
 
 export default async function LeagueLeaders() {
-    try {
-        const { data }: LeagueLeadersApi.Response =
-            await axios.get('/api/leagueLeaders');
-        return <div>{JSON.stringify(data)}</div>;
-    } catch (e) {
-        return <div>{JSON.stringify(e)}</div>;
-    }
+    const { data }: LeagueLeadersApi.Response =
+        await axios.get('/api/leagueLeaders');
+    // try {
+    //         return <div>{JSON.stringify(data)}</div>;
+    // } catch (e) {
+    //     return <div>{JSON.stringify(e)}</div>;
+    // }
 
+    return (
+        <>
+            <Subheader text='League Leaders' />
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Player</TableCell>
+                            <TableCell>Owner</TableCell>
+                            <TableCell align='right'>Home Runs</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {data.map(({ playerId, fullName, owner, homeRuns }) => (
+                            <LeagueLeaderRow
+                                key={playerId}
+                                playerId={playerId}
+                                fullName={fullName}
+                                teamKey={owner}
+                                homeRuns={homeRuns}
+                            />
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
+    );
 }
