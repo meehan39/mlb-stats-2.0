280307b886347d75b048e28691605bc0f5fe5857
Brendan Meehan <bmeehan@ebsco.com>
2024-03-25 16:05:08 -0400

    tweak header padding, spacing, font

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,9 +5,10 @@ export default function Header() {
         <div className='w-full items-center justify-center flex'>
             <a
                 href='/'
-                className='flex w-full justify-around items-center group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-slate-700 hover:bg-slate-800/30'>
+                className='flex w-full justify-between items-center group rounded-lg border border-transparent pb-4 transition-colors hover:border-slate-700 hover:bg-slate-800/30'>
                 <Image src='/JV-main1.PNG' alt='' width={50} height={50} />
-                <h1 className={`text-2xl font-semibold whitespace-nowrap`}>
+                <h1
+                    className={`text-2xl font-semibold whitespace-nowrap font-['Verdana']`}>
                     Home Run Verdi
                 </h1>
                 <Image src='/JV-main2.PNG' alt='' width={50} height={50} />
