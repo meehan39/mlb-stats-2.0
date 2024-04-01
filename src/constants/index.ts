7547ab6cff9053e688ed348debb1fdee4e204289
Brendan Meehan <bmeehan@ebsco.com>
2024-03-31 20:11:59 -0400

    fix seaon declaration

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 import type { LeagueData, TimeSpan } from './types';
 
-export const { SEASON } = process.env ?? 2024;
+export const SEASON = process.env.SEASON ?? '2023';
 export const LEAGUE_DATA: LeagueData = {
     matt: {
         teamName: 'Matty Hends',
