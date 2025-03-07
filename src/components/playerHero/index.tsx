5a8c29cbf1fa87efd6183aad9e3a5b34df47cd27
Brendan Meehan <bmeehan@ebsco.com>
2025-03-07 11:44:56 -0500

    update hero styling

--- src/components/playerHero/index.tsx
+++ src/components/playerHero/index.tsx
@@ -12,70 +12,67 @@ export default function PlayerHero({player, className, children}: PlayerHero.Pro
   const router = useRouter();
   const isLoading = !player;
   return (
-<div
-  onClick={() => {
-    if (!isLoading && player) {
-      router.push(`/player/${player.playerId}`);
-    }
-  }}
-  className={`clickable-card`}>
-  <Image
-    src={
-      !isLoading && player
-        ? PATHS.PLAYER_HERO_IMAGE(player.playerId)
-        : '/generic-avatar.PNG'
-    }
-    alt={player?.name ?? 'Generic player avatar'}
-    width={100}
-    height={150}
-    className={`h-56 w-32 rounded-lg object-cover raised ${isLoading && 'animate-pulse'}`}
-  />
-  <div className='flex flex-col gap-1 w-full'>
-    <Loading isLoading={isLoading} text='xl' width='w-44 md:w-52'>
-      <span className={`text-xl font-medium`}>
-        {/* {[data[i].name, primaryNumber && `#${primaryNumber}`] */}
-        {player?.name}
-      </span>
-    </Loading>
-    <Loading isLoading={isLoading} text='sm' width='w-32'>
-      <span className='text-sm font-medium secondary-text'>
-        {player?.teamName}
-      </span>
-    </Loading>
-    <div className={`w-full h-full p-2 ${className}`}>
-      {children}
-    </div>
-    <div className='divider-horizontal'></div>
-    <div className='h-full flex gap-1 w-full justify-center items-center'>
-      {player?.game ? (
-        <div className='flex flex-col h-full w-full justify-between'>
-          <div className='text-sm secondary-text whitespace-nowrap flex justify-between w-full'>
-            <span>{getGameState(player?.game)}</span>
-            <span>
-              {player.game.state !== 'scheduled' &&
-                `${player.game.homeRuns} HR${player.game.homeRuns !== 1 ? 's' : ''}`}
-            </span>
-          </div>
-          <div className='text-sm flex flex-col justify-between h-full w-full py-1'>
-            <ScoreBugItem
-              team={player.game.home}
-              state={player.game.state}
-            />
-            <ScoreBugItem
-              team={player.game.away}
-              state={player.game.state}
-            />
-          </div>
+    <div
+      onClick={() => {
+        if (!isLoading && player) {
+          router.push(`/player/${player.playerId}`);
+        }
+      }}
+      className={`clickable-card`}>
+      <Image
+        src={
+          !isLoading && player
+            ? PATHS.PLAYER_HERO_IMAGE(player.playerId)
+            : '/generic-avatar.PNG'
+        }
+        alt={player?.name ?? 'Generic player avatar'}
+        width={100}
+        height={150}
+        className={`h-56 w-32 rounded-lg object-cover raised ${isLoading && 'animate-pulse'}`}
+      />
+      <div className='flex flex-col gap-1 w-full'>
+        <Loading isLoading={isLoading} text='xl' width='w-44 md:w-52'>
+          <span className={`text-xl font-medium`}>
+            {/* {[data[i].name, primaryNumber && `#${primaryNumber}`] */}
+            {player?.name}
+          </span>
+        </Loading>
+        <Loading isLoading={isLoading} text='sm' width='w-32'>
+          <span className='text-sm font-medium secondary-text'>
+            {player?.teamName}
+          </span>
+        </Loading>
+        <div className={`w-full h-full p-2 ${className}`}>{children}</div>
+        <div className='h-full flex gap-1 w-full justify-center items-center'>
+          {player?.game ? (
+            <div className='flex flex-col gap-1 h-full w-full justify-between'>
+              <div className='text-sm secondary-text whitespace-nowrap flex justify-between w-full'>
+                <span>{getGameState(player?.game)}</span>
+                <span>
+                  {player.game.state !== 'scheduled' &&
+                    `${player.game.homeRuns} HR${player.game.homeRuns !== 1 ? 's' : ''}`}
+                </span>
+              </div>
+              <div className='text-sm flex flex-col gap-1 justify-between h-full w-full'>
+                <ScoreBugItem
+                  team={player.game.home}
+                  state={player.game.state}
+                />
+                <ScoreBugItem
+                  team={player.game.away}
+                  state={player.game.state}
+                />
+              </div>
+            </div>
+          ) : isLoading ? (
+            <Spinner />
+          ) : (
+            <span className='text-sm secondary-text'>No game today.</span>
+          )}
         </div>
-      ) : isLoading ? (
-        <Spinner />
-      ) : (
-        <span className='text-sm secondary-text'>No game today.</span>
-      )}
+      </div>
     </div>
-  </div>
-</div>
-  )
+  );
 }
 
 export function ScoreBugItem({
